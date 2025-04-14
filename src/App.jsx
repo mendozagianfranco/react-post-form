b723a3e8c970f07b1d6f667a4481d1b2ae0a5e9f
mendozagianfranco <mendozagianfranco88@gmail.com>
2025-04-14 15:07:18 +0200

    feat: complete structure html form

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,22 @@ function App() {
   return (
     <>
       <form>
-        <input type="text" />
-        <input type="text" />
-        <input type="text" />
-        <input type="checkbox" />
+        <div>
+          <label htmlFor="author-element">Nome Autore</label>
+          <input type="text" name='author' id='author-element' placeholder='Inserire nome autore' />
+        </div>
+        <div>
+          <label htmlFor="title-element">Titolo Post</label>
+          <input type="text" name='title' id='title-element' placeholder='Inserire Titolo post' />
+        </div>
+        <div>
+          <label htmlFor="body-element">Contenuto post</label>
+          <input type="text" name='body' id='body-element' placeholder='Inserire contenuto post' />
+        </div>
+        <div>
+          <label htmlFor="public-element">Pubblicare</label>
+          <input type="checkbox" id='public-element' name='public' />
+        </div>
         <button>Crea Post</button>
       </form>
     </>
