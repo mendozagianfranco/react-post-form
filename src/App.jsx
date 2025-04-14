bd22e2fa6220bc5cc78b971d296cd084c8319901
mendozagianfranco <mendozagianfranco88@gmail.com>
2025-04-14 14:53:59 +0200

    feat: create stucture form

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
 function App() {
   return (
     <>
-
+      <form>
+        <input type="text" />
+        <input type="text" />
+        <input type="text" />
+        <input type="checkbox" />
+        <button>Crea Post</button>
+      </form>
     </>
   );
 }
